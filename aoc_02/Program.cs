4cf9e4d8c6e5cf25f06e1adae140a8b413ea9eb4
Daniel Draper <DraperDanMan@users.noreply.github.com>
2022-12-13 20:59:19 +1100

    Day 2 Part 1 & 2

---
+++ aoc_02/Program.cs
@@ -0,0 +1,100 @@
+﻿//#define PART_ONE
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+internal class Program
+{
+    public static void Part1(string filepath)
+    {
+        int totalPoints = 0;
+
+        var outcomeLookup = new Dictionary<string, int>()
+        {
+            { "AX", 4 },
+            { "AY", 8 },
+            { "AZ", 3 },
+
+            { "BX", 1 },
+            { "BY", 5 },
+            { "BZ", 9 },
+
+            { "CX", 7 },
+            { "CY", 2 },
+            { "CZ", 6 },
+        };
+        
+        using (var file = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+        {
+            using (var reader = new StreamReader(file))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    string combined = line.Replace(" ", "");
+                    totalPoints += outcomeLookup[combined];
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        
+        Console.Out.WriteLine($"Total Score: {totalPoints}");
+    }
+    
+    public static void Part2(string filepath)
+    {
+        int totalPoints = 0;
+
+        var outcomeLookup = new Dictionary<string, int>()
+        {
+            { "AX", 3 },
+            { "AY", 4 },
+            { "AZ", 8 },
+
+            { "BX", 1 },
+            { "BY", 5 },
+            { "BZ", 9 },
+
+            { "CX", 2 },
+            { "CY", 6 },
+            { "CZ", 7 },
+        };
+        
+        using (var file = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+        {
+            using (var reader = new StreamReader(file))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    string combined = line.Replace(" ", "");
+                    totalPoints += outcomeLookup[combined];
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        
+        Console.Out.WriteLine($"Total Score: {totalPoints}");
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            return;
+        }
+
+        FileInfo fileInfo = new FileInfo(args[0]);
+        if (!fileInfo.Exists)
+        {
+            return;
+        }
+        
+#if PART_ONE
+        Part1(args[0]);
+#else
+        Part2(args[0]);
+#endif
+    }
+}
